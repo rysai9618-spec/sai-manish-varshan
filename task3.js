5a4d9bf7c3d3c5e471f1307786d3d50d2b74d012
rysai9618-spec <rysai9618@gmail.com>
2026-02-06 15:29:31 +0530

    Add files via upload

--- task3.js
+++ task3.js
@@ -1,9 +1,7 @@
-/ Step 1: const variable
-const num = 10;
-function test() {
-    num = 20;          
-    console.log(num);
+let num = -7;
+
+if (num >= 0) {
+  console.log("Positive number");
+} else {
+  console.log("Negative number");
 }
-test();  
-num = 30;             
-console.log(num);
