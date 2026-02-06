2a4dd952a0d7c91f1891232335c6d6f52943e5cd
rysai9618-spec <rysai9618@gmail.com>
2026-02-06 15:28:39 +0530

    Add files via upload

---
+++ task2.js
@@ -0,0 +1,13 @@
+let a = 10;
+let b = 3;
+
+console.log(a + b);   
+console.log(a - b);   
+console.log(a * b);  
+console.log(a / b);   
+console.log(a % b);   
+
+console.log(a == "10");   
+console.log(a === "10");  
+console.log(a > 5 && b < 5); 
+console.log(a < 5 || b < 5); 
