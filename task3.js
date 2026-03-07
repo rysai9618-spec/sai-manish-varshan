a814cc0c08a82c4eead008c45dc6c1f3be761b98
rysai9618-spec <rysai9618@gmail.com>
2026-03-07 15:24:22 +0530

    Add files via upload

--- task3.js
+++ task3.js
@@ -1,7 +1,16 @@
-let num = -7;
+function checkDiscount(Member, Amount) {
+    let discount = 0;
 
-if (num >= 0) {
-  console.log("Positive number");
-} else {
-  console.log("Negative number");
+    if (Member) {
+        discount = Amount * 0.30; 
+        let finalAmount = Amount - discount;
+        console.log("Customer is a member.");
+        console.log("Discount: " + discount);
+        console.log("Final Amount: " + finalAmount);
+    } else {
+        console.log("Customer is not a member.");
+        console.log("No discount.");
+        console.log("Amount to pay: " + Amount);
+    }
 }
+checkDiscount(true, 1000);
